667c831cff77ac19043d8171560f421be5ecd35b
walterdry <bardaleswa@gmail.com>
2019-11-20 23:40:56 -0600

    Add files via upload

--- Funciones/customTests.R
+++ Funciones/customTests.R
@@ -56,8 +56,8 @@ test_func5 <- function() {
 test_func6 <- function() {
   try({
     func <- get('mad_libs', globalenv())
-    t1 <- identical(func(lugar = "Baltimore", adjetivo = "smelly", sustantivo = "Roger Peng statue"), "Noticias desde Baltimore en la actualidad, donde los estudiantes smelly salieron a las calles en protesta por el nuevo Roger Peng statue que se está instalando en el campus.")
-    t2 <- identical(func(lugar = "Washington", adjetivo = "angry", sustantivo = "Shake Shack"), "Noticias desde Washington en la actualidad, donde los estudiantes angry salieron a las calles en protesta por el nuevo Shake Shack que se está instalando en el campus.")
+    t1 <- identical(func(lugar = "Baltimore", adjetivo = "smelly", sustantivo = "Roger Peng statue"), "Noticias desde Baltimore en la actualidad, donde los estudiantes smelly salieron a las calles en protesta por el nuevo Roger Peng statue que se esta instalando en el campus.")
+    t2 <- identical(func(lugar = "Washington", adjetivo = "angry", sustantivo = "Shake Shack"), "Noticias desde Washington en la actualidad, donde los estudiantes angry salieron a las calles en protesta por el nuevo Shake Shack que se esta instalando en el campus.")
     ok <- all(t1, t2)
   }, silent = TRUE)
   exists('ok') && isTRUE(ok)
