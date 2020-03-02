3a07009219c16787168b4e3f61ea76cc5cda1269
Walter Bardales <bardaleswa@gmail.com>
2020-03-01 23:33:32 -0600

    Update initLesson.R

--- Visualizacion_de_datos/initLesson.R
+++ Visualizacion_de_datos/initLesson.R
@@ -1,6 +1,6 @@
 # Path to data
 .datapath <- file.path(path.package('swirl'), 'Courses',
-                      'Programando_en_R', 'Visualizacion_de_datos',
+                      'Introduccion_a_la_programacion_R', 'Visualizacion_de_datos',
                       'plant-data.txt')
 # Read in data
 plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
